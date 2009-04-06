6961953bc1675c46de9a6008de616549330b2767
coremayo <coremayo@9d6d42d8-18c8-11de-babb-ddc7aea8d8a1>
2009-04-06 20:47:51 +0000

    UnitTests added for DBUtil fixed problem with getTitle method

--- iRaptorPackage/DBUtil.java
+++ iRaptorPackage/DBUtil.java
@@ -1,12 +1,16 @@
+package iRaptorPackage;
 
-
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-/* TODO convert sql queries to use prepared statements
+/* converted sql queries to use prepared statements
  * useful website about prepared statements and sql injection:
  * http://www.owasp.org/index.php/Preventing_SQL_Injection_in_Java
  */
@@ -57,7 +61,7 @@ public class DBUtil {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection(
-					"jdbc:sqlite:" + fileName.getName());
+					"jdbc:sqlite:" + fileName.getAbsolutePath());
 			
 			PreparedStatement ps = conn.prepareStatement(sqltxt);
 			ps.setString(1, title);
@@ -111,7 +115,7 @@ public class DBUtil {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection(
-					"jdbc:sqlite:" + fileName.getName());
+					"jdbc:sqlite:" + fileName.getAbsolutePath());
 			
 			PreparedStatement ps = conn.prepareStatement(sqltxt);
 			ps.setString(1, title);
@@ -153,7 +157,7 @@ public class DBUtil {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection(
-					"jdbc:sqlite:" + fileName.getName());
+					"jdbc:sqlite:" + fileName.getAbsolutePath());
 			
 			PreparedStatement ps = conn.prepareStatement(sqltxt);
 			ps.setInt(1, itemId);
@@ -214,7 +218,7 @@ public class DBUtil {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection(
-					"jdbc:sqlite:" + fileName.getName());
+					"jdbc:sqlite:" + fileName.getAbsolutePath());
 			
 			PreparedStatement ps = conn.prepareStatement(sqltxt);
 			ps.setInt(1, itemId);
@@ -270,7 +274,7 @@ public class DBUtil {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection(
-					"jdbc:sqlite:" + fileName.getName());
+					"jdbc:sqlite:" + fileName.getAbsolutePath());
 			
 			PreparedStatement ps = conn.prepareStatement(sqltxt);
 			ps.setString(1, publisher);
@@ -311,7 +315,7 @@ public class DBUtil {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection(
-					"jdbc:sqlite:" + fileName.getName());
+					"jdbc:sqlite:" + fileName.getAbsolutePath());
 			
 			PreparedStatement ps = conn.prepareStatement(sqltxt);
 			ps.setInt(1, itemId);
@@ -340,7 +344,7 @@ public class DBUtil {
 	 * @return that particular item's title or empty string if problems arise
 	 */
 	public static String getTitle(int itemId) {
-		//this sql query will get the title for the given itemId
+		//this SQL query will get the title for the given itemId
 		String sqltxt = 
 			 "SELECT title FROM Item WHERE itemId = ?;";
 		
@@ -348,15 +352,17 @@ public class DBUtil {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection(
-					"jdbc:sqlite:" + fileName.getName());
+					"jdbc:sqlite:" + fileName.getAbsolutePath());
 			PreparedStatement ps = conn.prepareStatement(sqltxt);
+			ps.setInt(1, itemId);
 			
 			//this will return the first record in the "title" column
 			return ps.executeQuery().getString("title");
 		}
 		catch (Exception e) {
-			System.err.println("getTitle function failed");
-			System.err.println(e.getMessage());
+			System.err.println("getTitle function failed while trying to get " +
+					"title for item: " + itemId);
+			e.printStackTrace();
 		}
 		finally {
 			if (conn != null) {
@@ -385,7 +391,8 @@ public class DBUtil {
 		Connection conn = null; //same stuff to set up connection
 		try {
 			Class.forName("org.sqlite.JDBC");
-			conn = DriverManager.getConnection("jdbc:sqlite:" + fileName.getName());
+			conn = DriverManager.getConnection("jdbc:sqlite:" + 
+					fileName.getAbsolutePath());
 			PreparedStatement ps = conn.prepareStatement(sqltxt);
 			ret = ps.executeQuery().getInt("itemId");
 		}
@@ -404,5 +411,126 @@ public class DBUtil {
 			}
 		}
 		return ret;
+	}
+
+	/**
+	 * The setUpDatabase() method will run all CREATE TABLE statements on a 
+	 * newly created file so that it can be used by the application
+	 */
+	public static void setUpDatabase() {
+		Connection conn = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:" + 
+					fileName.getAbsolutePath());
+			
+			File file = new File("createTables.sql");
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			
+			SqlScript script = new DBUtil().new SqlScript(br, conn);
+			
+			script.loadScript();
+			script.execute();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Will execute a SQL script file on a connection
+	 * 
+	 * taken from
+	 * http://www.sternsquirt.com/blog/?cat=10
+	 *
+	 */
+	public class SqlScript {
+
+		public final static char QUERY_ENDS = ';';
+
+		private Connection conn;
+		private BufferedReader rdr;
+		private Statement statement;
+
+		/**
+		 * Constructor: takes a bufferedReader and a sql connection to create the SqlScript object.
+		 * Note that construction does not automatically read the script.
+		 * @param bufRdr BufferedReader to the script data.
+		 * @param connection SQL Connection
+		 * @throws SQLException
+		 */
+		public SqlScript(BufferedReader bufRdr, Connection connection) throws SQLException {
+			rdr = bufRdr;
+			conn = connection;
+			statement = conn.createStatement();
+		}
+
+		/**
+		 * Loads the Sql Script from the BufferedReader and parses it into a statement.
+		 * @throws IOException
+		 * @throws SQLException
+		 */
+		public void loadScript() throws IOException, SQLException {
+			String line;
+			StringBuffer query = new StringBuffer();
+			boolean queryEnds = false;
+
+			while ((line = rdr.readLine()) != null) {
+				if (isComment(line))
+					continue;
+				queryEnds = checkStatementEnds(line);
+				query.append(line);
+				if (queryEnds) {
+					statement.addBatch(query.toString());
+					query.setLength(0);
+				}
+			}
+		}
+
+		/**
+		 * @param line
+		 * @return
+		 */
+		private boolean isComment(String line) {
+			if ((line != null) && (line.length() > 0))
+				return (line.charAt(0) == '#');
+			return false;
+		}
+
+		/**
+		 * Executes the statement created by loadScript.
+		 *
+		 * @throws IOException
+		 * @throws SQLException
+		 */
+		public void execute() throws IOException, SQLException {
+			statement.executeBatch();
+		}
+
+		private boolean checkStatementEnds(String s) {
+			return (s.indexOf(QUERY_ENDS) != -1);
+		}
+
+		/**
+		 * @return the statement
+		 */
+		public Statement getStatement() {
+			return statement;
+		}
+
+		/**
+		 * @param statement the statement to set
+		 */
+		public void setStatement(Statement statement) {
+			this.statement = statement;
+		}
 	}
 }
--- iRaptorTest/DBUtilTest.java
+++ iRaptorTest/DBUtilTest.java
@@ -1,37 +1,93 @@
 package iRaptorTest;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import iRaptorPackage.DBUtil;
 
 public class DBUtilTest {
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		DBUtil.fileName = File.createTempFile("RaptorUnitTests", ".db");
+		System.out.println("Testing DBUtil using file: " + 
+				DBUtil.fileName.getAbsolutePath());
+		DBUtil.setUpDatabase();
+		
+		//add a few books to test some stuff
+		DBUtil.addBook("book1", "genre1", 5, 5, "none", "none", null);
+		DBUtil.addBook("Book2", "genre2", 6, 6, null, "blah", null);
+	}
+	
+	@Before
+	public void setUp() throws Exception { }
 
 	@Test
 	public void testAddBook() {
-		if((DBUtil.addBook("a new book", "fiction", 5, 1999, "good read", 
-				"random ass books", "1234")) < 0) { 
+		int ret = DBUtil.addBook("a new book", "fiction", 5, 1999, "good read", 
+				"random ass books", "1234");
+		if (ret < 0) {
 			fail("Returned negative value");
 		}
+		
+		System.out.println("--addBook test finished");
 	}
 
 	@Test
 	public void testUpdateBook() {
-		fail("Not yet implemented");
+		if ((DBUtil.updateBook(DBUtil.mostRecentItem(), "this is a new title", 
+				"non-fiction", 5, 2008, "not as good", "nope", "4321")) < 0){
+			fail("returned negative value");
+		}
+		
+		System.out.println("--updateBook test finished");
 	}
 
 	@Test
 	public void testRemoveBook() {
-		fail("Not yet implemented");
+		int itemId = DBUtil.mostRecentItem();
+		int ret = DBUtil.removeBook(itemId);
+		if (ret < 0) {
+			fail("returned negative value");
+		}
+		
+		System.out.println("--removeBook test finished");
 	}
 
 	@Test
 	public void testGetTitle() {
-		fail("Not yet implemented");
+		String title = DBUtil.getTitle(DBUtil.mostRecentItem());
+		if (title == "") {
+			fail("returned empty string");
+		}
+
+		System.out.println("--getTitle test finished");
 	}
 
 	@Test
 	public void testMostRecentItem() {
-		fail("Not yet implemented");
+		String title = "newest book";
+		int itemId = DBUtil.addBook(title, null, 5, 5, null, null, null);
+		int newItemId = DBUtil.mostRecentItem();
+		if (newItemId != itemId) {
+			fail("most recent item was not returned");
+		}
+		
+		System.out.println("--mostRecentItem test finished");
+	}
+	
+	@After
+	public void tearDown() throws Exception { }
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("Done with testing DBUtil");
 	}
 
 }
